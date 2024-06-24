b7d7e0052ee5d113c94105ef27376c279e4fbb32
Marcio de Andrade <marcio@requestdev.net>
2024-06-24 13:56:11 -0400

    Criação da classe StudentCommand

---
+++ CQRSAndMediatRDemo/Commands/CreateStudentCommand.cs
@@ -0,0 +1,20 @@
+﻿using CQRSAndMediatRDemo.Models;
+using MediatR;
+
+namespace CQRSAndMediatRDemo.Commands
+{
+    public class CreateStudentCommand : IRequest<StudentDetails>
+    {
+        public string StudentName { get; set; }
+        public string StudentEmail { get; set; }
+        public string StudentAddress { get; set; }
+        public int StudentAge { get; set; }
+        public CreateStudentCommand(string studentdName, string studentEmail, string studentAddress, int studentAge)
+        {
+            StudentName = studentdName;
+            StudentEmail = studentEmail;
+            StudentAddress = studentAddress;
+            StudentAge = studentAge;
+        }
+    }
+}
